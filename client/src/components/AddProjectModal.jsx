05fb3d25cc8b0c8a356f30126b6722c1eec7f674
AtharvaCM <necrons999@gmail.com>
2022-12-08 12:03:46 +0530

    addProject mutation added

---
+++ client/src/components/AddProjectModal.jsx
@@ -0,0 +1,170 @@
+import { useMutation, useQuery } from "@apollo/client";
+import { useState } from "react";
+import { FaList } from "react-icons/fa";
+
+import { GET_PROJECTS } from "../queries/projectQueries";
+import { GET_CLIENTS } from "../queries/clientQueries";
+import { ADD_PROJECT } from "../mutations/projectMutation";
+
+const AddProjectModal = () => {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [clientId, setClientId] = useState("");
+  const [status, setStatus] = useState("new");
+
+  const [addProject] = useMutation(ADD_PROJECT, {
+    variables: { name, description, status, clientId },
+    update(cache, { data: { addProject } }) {
+      const { projects } = cache.readQuery({ query: GET_PROJECTS });
+
+      cache.writeQuery({
+        query: GET_PROJECTS,
+        data: { projects: [...projects, addProject] },
+      });
+    },
+  });
+
+  // Get clients for select
+  const { loading, error, data } = useQuery(GET_CLIENTS);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, description, status);
+    if (name === "" || description === "" || status === "") {
+      return alert("Please fill all fields");
+    }
+
+    addProject(name, description, status, clientId);
+
+    setName("");
+    setDescription("");
+    setStatus("new");
+    setClientId("");
+  };
+
+  if (loading) return null;
+  if (error) return "Something Went Wrong";
+
+  return (
+    <>
+      {!loading && !error && (
+        <>
+          {/* <!-- Button trigger modal --> */}
+          <button
+            type="button"
+            className="btn btn-primary"
+            data-bs-toggle="modal"
+            data-bs-target="#addProjectModal"
+          >
+            <div className="d-flex align-items-center">
+              <FaList className="icon" />
+              <div>New Project</div>
+            </div>
+          </button>
+
+          {/* <!-- Modal --> */}
+          <div
+            className="modal fade"
+            id="addProjectModal"
+            aria-labelledby="addProjectModalLabel"
+            aria-hidden="true"
+          >
+            <div className="modal-dialog">
+              <div className="modal-content">
+                {/* Modal Header */}
+                <div className="modal-header">
+                  <h5 className="modal-title" id="addProjectModalLabel">
+                    New Project
+                  </h5>
+                  <button
+                    type="button"
+                    className="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                  ></button>
+                </div>
+                {/* Modal Body */}
+                <div className="modal-body">
+                  <form action="" onSubmit={handleSubmit}>
+                    <div className="mb-3">
+                      <label className="form-label">Name</label>
+                      <input
+                        type={"text"}
+                        className="form-control"
+                        id="name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <label className="form-label">Description</label>
+                      <textarea
+                        className="form-control"
+                        id="description"
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <label className="form-label">Status</label>
+                      <select
+                        className="form-select"
+                        id="status"
+                        value={status}
+                        onChange={(e) => setStatus(e.target.value)}
+                      >
+                        <option value="new">Not Started</option>
+                        <option value="progress">In Progress</option>
+                        <option value="completed">Completed</option>
+                      </select>
+                    </div>
+
+                    <div className="mb-3">
+                      <label className="form-label">Client</label>
+                      <select
+                        className="form-select"
+                        id="clientId"
+                        value={clientId}
+                        onChange={(e) => setClientId(e.target.value)}
+                      >
+                        <option value="">Select Client</option>
+                        {data.clients.map((client) => (
+                          <option key={client.id} value={client.id}>
+                            {client.name}
+                          </option>
+                        ))}
+                      </select>
+                    </div>
+
+                    <button
+                      type="submit"
+                      data-bs-dismiss="modal"
+                      className="btn btn-primary"
+                    >
+                      Submit
+                    </button>
+                  </form>
+                </div>
+                {/* Modal Footer */}
+                <div className="modal-footer">
+                  <button
+                    type="button"
+                    className="btn btn-secondary"
+                    data-bs-dismiss="modal"
+                  >
+                    Close
+                  </button>
+                  <button type="button" className="btn btn-primary">
+                    Save changes
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+
+export default AddProjectModal;

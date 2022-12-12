135182e702b7463b00838932789d4dd790ed53b5
AtharvaCM <necrons999@gmail.com>
2022-12-12 09:14:35 +0530

    editProject form added

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -6,6 +6,7 @@ import { GET_PROJECT } from "../queries/projectQueries";
 import { ClientInfo } from "../components/ClientInfo";
 import Spinner from "../components/Spinner";
 import DeleteProjectButton from "../components/DeleteProjectButton";
+import EditProjectForm from "../components/EditProjectForm";
 
 const Project = () => {
   const { id } = useParams();
@@ -33,6 +34,8 @@ const Project = () => {
           <p className="lead">{data.project.status}</p>
 
           <ClientInfo client={data.project.client} />
+
+          <EditProjectForm project={data.project} />
 
           <DeleteProjectButton projectId={data.project.id} />
         </div>
